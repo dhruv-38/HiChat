5badba6dba9e93110c3713812f0f29ceba5c0200
Dhruv Choudhary <dhruvchoudhary38@gmail.com>
2025-12-11 12:10:39 +0530

    some code refractor in utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,17 +1,22 @@
 import jwt from "jsonwebtoken";
+import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
-    const { JWT_SECRET } = ENV;
-    if (!JWT_SECRET) {
-        throw new Error("JWT_SECRET is not configured");
-    }
-    const token = jwt.sign({ userId }, JWT_SECRET, { expiresIn: "7d" });
-    res.cookie("jwt", token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true, // prevent XSS attacks: Cross-Site Scripting
-        sameSite: "strict",// prevent CSRF attacks: Cross-Site Request Forgery
-        secure: process.env.NODE_ENV === "development" ? false : true, // only send cookie over HTTPS in production
-    });
+  const { JWT_SECRET } = ENV;
+  if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is not configured");
+  }
 
-    return token;
-}
+  const token = jwt.sign({ userId }, JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+    httpOnly: true, // prevent XSS attacks: cross-site scripting
+    sameSite: "strict", // CSRF attacks
+    secure: ENV.NODE_ENV === "development" ? false : true,
+  });
+
+  return token;
+};
