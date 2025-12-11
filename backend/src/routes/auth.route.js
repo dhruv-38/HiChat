abb740154f96e775a76003bafc22c474e428c833
Dhruv Choudhary <146524483+dhruv-38@users.noreply.github.com>
2025-12-11 19:19:27 +0530

    Revert "login and logout functionality is added"

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { signup } from "../controllers/auth.controller.js";
 
 const router =express.Router();
 
 router.post("/signup",signup);
-router.post("/login", login);
-router.post("/logout", logout);
+
+router.get("/login",(req,res)=>{
+    res.send("Hello from login route");
+});
+
+router.get("/logout",(req,res)=>{
+    res.send("Hello from logout route");
+});
 
 export default router;
